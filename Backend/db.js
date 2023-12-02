298d8163ef281923b9306de414fd081a0005b3ef
SubhanKhurshid <105592966+SubhanKhurshid@users.noreply.github.com>
2023-12-02 16:44:37 +0500

    h

--- Backend/db.js
+++ Backend/db.js
@@ -4,6 +4,6 @@ export const db = mysql.createPool({
   connectionLimit: 1000,
   host: "localhost",
   user: "root",
-  password: "subhan123",
+  password: "Saadrehman123@@!",
   database: "event",
 });
